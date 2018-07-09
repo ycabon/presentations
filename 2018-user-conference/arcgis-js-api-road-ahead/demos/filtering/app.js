89093091f3dd54f2791b31ef27fac8f9e59443f2
Yann Cabon <ycabon@esri.com>
2018-07-09 11:13:26 -0700

    update filtering app

--- 2018-user-conference/arcgis-js-api-road-ahead/demos/filtering/app.js
+++ 2018-user-conference/arcgis-js-api-road-ahead/demos/filtering/app.js
@@ -33,10 +33,12 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-define(["require", "exports", "esri/config", "esri/layers/CSVLayer", "esri/renderers", "esri/symbols", "esri/WebMap", "esri/widgets/Legend", "esri/widgets/Expand", "esri/views/MapView", "esri/core/watchUtils"], function (require, exports, esriConfig, CSVLayer, renderers_1, symbols_1, WebMap, Legend, Expand, MapView, watchUtils) {
+define(["require", "exports", "esri/config", "esri/layers/CSVLayer", "esri/renderers", "esri/symbols", "esri/WebMap", "esri/widgets/Legend", "esri/widgets/Expand", "esri/views/MapView", "esri/core/watchUtils", "esri/layers/GraphicsLayer", "esri/widgets/Sketch/SketchViewModel", "esri/Graphic", "esri/tasks/support/Query", "@dojo/core/util", "@dojo/shim/Set"], function (require, exports, esriConfig, CSVLayer, renderers_1, symbols_1, WebMap, Legend, Expand, MapView, watchUtils, GraphicsLayer, SketchViewModel, Graphic, Query, util_1, Set_1) {
     var _this = this;
     Object.defineProperty(exports, "__esModule", { value: true });
     esriConfig.request.corsEnabledServers.push("https://arcgis.github.io");
+    var map;
+    var view;
     var url = "https://arcgis.github.io/arcgis-samples-javascript/sample-data/hurricanes.csv";
     (function () { return __awaiter(_this, void 0, void 0, function () {
         // When user drags the slider:
@@ -51,25 +53,118 @@ define(["require", "exports", "esri/config", "esri/layers/CSVLayer", "esri/rende
         function setYear(value) {
             sliderValue.innerHTML = "" + Math.floor(value);
             slider.value = "" + Math.floor(value);
-            layerView;
-            layer
-                .queryObjectIds({
-                where: "year <> " + value
-            })
-                .then(function (objectIds) {
-                featuresView.setVisibility(hiddenIds, objectIds);
-                hiddenIds = objectIds;
-            })
-                .catch(function (error) {
-                console.error(error);
-            });
+            query.where = "year = " + value;
+            invalidateQuery();
         }
-        var layer, map, view, legendExpand, layerView, featuresView, hiddenIds, slider, sliderValue;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
+        function setupDrawing() {
+            view.ui.add(document.getElementById("topbar"), "top-right");
+            var editGraphic;
+            // GraphicsLayer to hold graphics created via sketch view model
+            var drawLayer = new GraphicsLayer();
+            map.add(drawLayer);
+            // create a new sketch view model
+            var sketchViewModel = new SketchViewModel({
+                view: view,
+                polygonSymbol: new symbols_1.SimpleFillSymbol({
+                    color: "rgba(0,0,0,0)",
+                    style: "solid",
+                    outline: {
+                        color: "white",
+                        width: 2
+                    }
+                })
+            });
+            setUpClickHandler();
+            sketchViewModel.on("create-complete", addGraphic);
+            sketchViewModel.on("create", updateQuery);
+            sketchViewModel.on("scale", updateQuery);
+            sketchViewModel.on("move", updateQuery);
+            sketchViewModel.on("rotate", updateQuery);
+            sketchViewModel.on("reshape", updateQuery);
+            sketchViewModel.on("update-complete", updateGraphic);
+            sketchViewModel.on("update-cancel", updateGraphic);
+            function updateQuery(event) {
+                query.geometry = event.geometry;
+                invalidateQuery();
+            }
+            function addGraphic(event) {
+                var graphic = new Graphic({
+                    geometry: event.geometry,
+                    symbol: sketchViewModel.graphic.symbol
+                });
+                drawLayer.add(graphic);
+            }
+            function updateGraphic(event) {
+                event.graphic.geometry = event.geometry;
+                drawLayer.add(event.graphic);
+                editGraphic = null;
+            }
+            //***************************************
+            // activate the sketch to create a polygon
+            //***************************************
+            var drawPolygonButton = document.getElementById("polygonButton");
+            drawPolygonButton.addEventListener("click", function () {
+                // set the sketch to create a polygon geometry
+                sketchViewModel.create("polygon");
+                setActiveButton(this);
+            });
+            //**************
+            // reset button
+            //**************
+            document.getElementById("resetBtn").onclick = function () {
+                sketchViewModel.reset();
+                drawLayer.removeAll();
+                setActiveButton();
+                query.geometry = null;
+                invalidateQuery();
+            };
+            function setActiveButton(selectedButton) {
+                // focus the view to activate keyboard shortcuts for sketching
+                view.focus();
+                var elements = document.getElementsByClassName("active");
+                for (var i = 0; i < elements.length; i++) {
+                    elements[i].classList.remove("active");
+                }
+                if (selectedButton) {
+                    selectedButton.classList.add("active");
+                }
+            }
+            // ************************************************************************************
+            // set up logic to handle geometry update and reflect the update on "tempGraphicsLayer"
+            // ************************************************************************************
+            function setUpClickHandler() {
+                view.on("click", function (event) {
+                    event.stopPropagation();
+                    view.hitTest(event).then(function (response) {
+                        var results = response.results;
+                        for (var _i = 0, results_1 = results; _i < results_1.length; _i++) {
+                            var graphic = results_1[_i].graphic;
+                            if (graphic.layer === drawLayer) {
+                                // Check if we're already editing a graphic
+                                if (!editGraphic) {
+                                    // Save a reference to the graphic we intend to update
+                                    editGraphic = graphic;
+                                    // Remove the graphic from the GraphicsLayer
+                                    // Sketch will handle displaying the graphic while being updated
+                                    drawLayer.remove(editGraphic);
+                                    sketchViewModel.update(editGraphic);
+                                }
+                                return;
+                            }
+                        }
+                    });
+                });
+            }
+        }
+        var layer, layerView, slider, sliderValue, allObjectIds, _a, featuresView, oldHiddenIds, query, invalidateQuery;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
                 case 0:
                     layer = new CSVLayer({
                         title: "Hurricanes",
+                        spatialReference: {
+                            wkt: "PROJCS[\"South Pole Stereographic_1\",GEOGCS[\"GCS WGS 1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Stereographic\"],PARAMETER[\"False_Easting\",0.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",-145.0],PARAMETER[\"Scale_Factor\",1.0],PARAMETER[\"Latitude_Of_Origin\",-90.0],UNIT[\"Meter\",1.0]]"
+                        },
                         url: url,
                         copyright: "NOAA",
                         popupTemplate: {
@@ -150,28 +245,54 @@ define(["require", "exports", "esri/config", "esri/layers/CSVLayer", "esri/rende
                             }
                         }
                     });
-                    legendExpand = new Expand({
+                    view.ui.add(new Expand({
                         view: view,
                         content: new Legend({
                             view: view,
                             style: "card"
                         })
-                    });
-                    view.ui.add(legendExpand, "top-left");
+                    }), "top-left");
+                    setupDrawing();
                     return [4 /*yield*/, view.whenLayerView(layer)];
                 case 1:
-                    layerView = (_a.sent());
+                    layerView = (_b.sent());
                     return [4 /*yield*/, watchUtils.whenDefinedOnce(layerView, "featuresView.tileRenderer")];
                 case 2:
-                    _a.sent();
-                    featuresView = layerView.featuresView.tileRenderer.featuresView;
-                    hiddenIds = [];
+                    _b.sent();
                     slider = document.getElementById("slider");
                     sliderValue = document.getElementById("sliderValue");
                     // const playButton = document.getElementById("playButton");
                     // const titleDiv = document.getElementById("titleDiv");
                     slider.addEventListener("input", inputHandler);
                     slider.addEventListener("change", inputHandler);
+                    return [4 /*yield*/, watchUtils.whenFalseOnce(layerView, "updating")];
+                case 3:
+                    _b.sent();
+                    _a = Set_1.default.bind;
+                    return [4 /*yield*/, layerView.queryObjectIds({ where: "1=1" })];
+                case 4:
+                    allObjectIds = new (_a.apply(Set_1.default, [void 0, _b.sent()]))();
+                    featuresView = layerView.featuresView.tileRenderer.featuresView;
+                    oldHiddenIds = [];
+                    query = new Query();
+                    invalidateQuery = util_1.throttleAfter(function () {
+                        layer
+                            .queryObjectIds(query)
+                            .then(function (objectIds) {
+                            var visibleSet = new Set_1.default(objectIds);
+                            var hiddenIds = [];
+                            allObjectIds.forEach(function (oid) {
+                                if (!visibleSet.has(oid)) {
+                                    hiddenIds.push(oid);
+                                }
+                            });
+                            featuresView.setVisibility(oldHiddenIds, hiddenIds);
+                            oldHiddenIds = hiddenIds;
+                        })
+                            .catch(function (error) {
+                            console.error(error);
+                        });
+                    }, 50);
                     return [2 /*return*/];
             }
         });
--- 2018-user-conference/arcgis-js-api-road-ahead/demos/filtering/app.ts
+++ 2018-user-conference/arcgis-js-api-road-ahead/demos/filtering/app.ts
@@ -1,15 +1,24 @@
 import esriConfig = require("esri/config");
 import CSVLayer = require("esri/layers/CSVLayer");
 import { UniqueValueRenderer } from "esri/renderers";
-import { PictureMarkerSymbol } from "esri/symbols";
+import { PictureMarkerSymbol, SimpleFillSymbol } from "esri/symbols";
 import WebMap = require("esri/WebMap");
 import Legend = require("esri/widgets/Legend");
 import Expand = require("esri/widgets/Expand");
 import MapView = require("esri/views/MapView");
 import CSVLayerView = require("esri/views/layers/CSVLayerView");
 import watchUtils = require("esri/core/watchUtils");
+import GraphicsLayer = require("esri/layers/GraphicsLayer");
+import SketchViewModel = require("esri/widgets/Sketch/SketchViewModel");
+import Graphic = require("esri/Graphic");
+import Query = require("esri/tasks/support/Query");
+import { throttleAfter } from "@dojo/core/util";
+import Set from "@dojo/shim/Set";
 
 esriConfig.request.corsEnabledServers.push("https://arcgis.github.io");
+
+let map: WebMap;
+let view: MapView;
 
 const url =
   "https://arcgis.github.io/arcgis-samples-javascript/sample-data/hurricanes.csv";
@@ -17,6 +26,9 @@ const url =
 (async () => {
   const layer = new CSVLayer({
     title: "Hurricanes",
+    spatialReference: {
+      wkt: `PROJCS["South Pole Stereographic_1",GEOGCS["GCS WGS 1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Stereographic"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-145.0],PARAMETER["Scale_Factor",1.0],PARAMETER["Latitude_Of_Origin",-90.0],UNIT["Meter",1.0]]`
+    },
     url: url,
     copyright: "NOAA",
     popupTemplate: {
@@ -75,7 +87,7 @@ const url =
     })
   });
 
-  const map = new WebMap({
+  map = new WebMap({
     // contains a basemap with a South Pole Stereographic projection
     // the CSVLayer coordinates will re-project client-side
     // with the Projection Engine to match the view's Spatial Reference
@@ -87,7 +99,7 @@ const url =
     layers: [layer]
   });
 
-  const view = new MapView({
+  view = new MapView({
     container: "viewDiv",
     map: map,
     highlightOptions: {
@@ -103,24 +115,22 @@ const url =
     }
   });
 
-  const legendExpand = new Expand({
-    view: view,
-    content: new Legend({
+  view.ui.add(
+    new Expand({
       view: view,
-      style: "card"
-    })
-  });
+      content: new Legend({
+        view: view,
+        style: "card"
+      })
+    }),
+    "top-left"
+  );
 
-  view.ui.add(legendExpand, "top-left");
+  setupDrawing();
 
   const layerView = (await view.whenLayerView(layer)) as CSVLayerView;
 
   await watchUtils.whenDefinedOnce(layerView, "featuresView.tileRenderer");
-
-  const featuresView: {
-    setVisibility(showFeatures: number[], hideFeatures: number[]): void;
-  } = (layerView as any).featuresView.tileRenderer.featuresView;
-  let hiddenIds: number[] = [];
 
   const slider = document.getElementById("slider") as HTMLInputElement;
   const sliderValue = document.getElementById("sliderValue");
@@ -137,25 +147,163 @@ const url =
     setYear(parseInt(slider.value, 10));
   }
 
+  await watchUtils.whenFalseOnce(layerView, "updating");
+
+  const allObjectIds = new Set(
+    await layerView.queryObjectIds({ where: "1=1" })
+  );
+
+  const featuresView: {
+    setVisibility(showFeatures: number[], hideFeatures: number[]): void;
+  } = (layerView as any).featuresView.tileRenderer.featuresView;
+  let oldHiddenIds: number[] = [];
+  const query = new Query();
+  const invalidateQuery = throttleAfter(() => {
+    layer
+      .queryObjectIds(query)
+      .then(objectIds => {
+        const visibleSet = new Set(objectIds);
+        const hiddenIds: number[] = [];
+        allObjectIds.forEach(oid => {
+          if (!visibleSet.has(oid)) {
+            hiddenIds.push(oid);
+          }
+        });
+
+        featuresView.setVisibility(oldHiddenIds, hiddenIds);
+        oldHiddenIds = hiddenIds;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, 50);
+
   /**
    * Sets the current visualized construction year.
    */
   function setYear(value: number): void {
     sliderValue.innerHTML = "" + Math.floor(value);
     slider.value = "" + Math.floor(value);
+    query.where = `year = ${value}`;
+    invalidateQuery();
+  }
 
-    layerView;
+  function setupDrawing() {
+    view.ui.add(document.getElementById("topbar"), "top-right");
 
-    layer
-      .queryObjectIds({
-        where: `year <> ${value}`
+    let editGraphic: Graphic;
+    // GraphicsLayer to hold graphics created via sketch view model
+    const drawLayer = new GraphicsLayer();
+
+    map.add(drawLayer);
+
+    // create a new sketch view model
+    const sketchViewModel = new SketchViewModel({
+      view: view,
+      polygonSymbol: new SimpleFillSymbol({
+        color: "rgba(0,0,0,0)",
+        style: "solid",
+        outline: {
+          color: "white",
+          width: 2
+        }
       })
-      .then(objectIds => {
-        featuresView.setVisibility(hiddenIds, objectIds);
-        hiddenIds = objectIds;
-      })
-      .catch(error => {
-        console.error(error);
+    });
+
+    setUpClickHandler();
+
+    sketchViewModel.on("create-complete", addGraphic);
+    sketchViewModel.on("create", updateQuery);
+    sketchViewModel.on("scale", updateQuery);
+    sketchViewModel.on("move", updateQuery);
+    sketchViewModel.on("rotate", updateQuery);
+    sketchViewModel.on("reshape", updateQuery);
+    sketchViewModel.on("update-complete", updateGraphic);
+    sketchViewModel.on("update-cancel", updateGraphic);
+
+    function updateQuery(event: any) {
+      query.geometry = event.geometry;
+      invalidateQuery();
+    }
+
+    function addGraphic(event: any) {
+      const graphic = new Graphic({
+        geometry: event.geometry,
+        symbol: sketchViewModel.graphic.symbol
       });
+
+      drawLayer.add(graphic);
+    }
+
+    function updateGraphic(event: any) {
+      event.graphic.geometry = event.geometry;
+      drawLayer.add(event.graphic);
+      editGraphic = null;
+    }
+
+    //***************************************
+    // activate the sketch to create a polygon
+    //***************************************
+    const drawPolygonButton = document.getElementById(
+      "polygonButton"
+    ) as HTMLButtonElement;
+    drawPolygonButton.addEventListener("click", function(
+      this: HTMLButtonElement
+    ) {
+      // set the sketch to create a polygon geometry
+      sketchViewModel.create("polygon");
+      setActiveButton(this);
+    });
+
+    //**************
+    // reset button
+    //**************
+    document.getElementById("resetBtn").onclick = function() {
+      sketchViewModel.reset();
+      drawLayer.removeAll();
+      setActiveButton();
+      query.geometry = null;
+      invalidateQuery();
+    };
+
+    function setActiveButton(selectedButton?: HTMLButtonElement) {
+      // focus the view to activate keyboard shortcuts for sketching
+      view.focus();
+      const elements = document.getElementsByClassName("active");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("active");
+      }
+      if (selectedButton) {
+        selectedButton.classList.add("active");
+      }
+    }
+
+    // ************************************************************************************
+    // set up logic to handle geometry update and reflect the update on "tempGraphicsLayer"
+    // ************************************************************************************
+    function setUpClickHandler() {
+      view.on("click", function(event) {
+        event.stopPropagation();
+
+        view.hitTest(event).then(response => {
+          const results = response.results;
+
+          for (const { graphic } of results) {
+            if (graphic.layer === drawLayer) {
+              // Check if we're already editing a graphic
+              if (!editGraphic) {
+                // Save a reference to the graphic we intend to update
+                editGraphic = graphic;
+                // Remove the graphic from the GraphicsLayer
+                // Sketch will handle displaying the graphic while being updated
+                drawLayer.remove(editGraphic);
+                sketchViewModel.update(editGraphic);
+              }
+              return;
+            }
+          }
+        });
+      });
+    }
   }
 })();
